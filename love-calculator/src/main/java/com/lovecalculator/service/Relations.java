4125a55bda2f07218a05846889aa6a7fbf280536
Daya8686 <dhondidaya8686@gmail.com>
2024-06-07 23:15:16 +0530

    Added Relation Values constants

---
+++ love-calculator/src/main/java/com/lovecalculator/service/Relations.java
@@ -0,0 +1,13 @@
+package com.lovecalculator.service;
+
+public class Relations {
+	
+	
+	public static final String  F_Meaning_value="Friends";
+	public static final String  L_Meaning_value="Love";
+	public static final String  A_Meaning_value="Affection";
+	public static final String  M_Meaning_value="Marriage";
+	public static final String  E_Meaning_value="Enemies";
+	public static final String  S_Meaning_value="Sister";
+
+}
